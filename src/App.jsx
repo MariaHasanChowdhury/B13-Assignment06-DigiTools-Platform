751315471bb49868158c6a98b839511198057206
MariaHasanChowdhury <mariahasanchowdhury1130@gmail.com>
2026-05-13 00:49:18 +0600

    delete something in app.css and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { Suspense, useState } from "react";
+import "./App.css";
+import Banner from "./Components/Banner/Banner";
+import Navbar from "./Components/Navbar/Navbar";
+import CardHolder from "./Components/Products/CardHolder/CardHolder";
+import Stat from "./Components/Stat/Stat";
+import Cart from "./Components/Cart/Cart";
+import GetStarted from "./Components/GetStarted/GetStarted";
+import PricingHolder from "./Components/Pricing/PricingHolder/PricingHolder";
+import Footer from "./Components/Footer/Footer";
+import { ToastContainer } from "react-toastify";
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
+
+  const [selected, setSelected] = useState("product");
+  const [cardIds, setCardIds] = useState([]);
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <header>
+        <Navbar cardIds={cardIds.length} />
+      </header>
+      <main>
+        <Banner />
+        <Stat />
+        <CardHolder
+          // CardPromise={CardPromise}
+          selected={selected}
+          setSelected={setSelected}
+          cardIds={cardIds}
+          setCardIds={setCardIds}
+        />
 
-      <div className="ticks"></div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        {
+          selected === "product" ?
+            <GetStarted />
+            : ""
+        }
+        {
+          selected === "product" ?
+            <PricingHolder />
+            : ""
+        }
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+        <Footer />
+
+        {/* react toastify */}
+        <ToastContainer />
+      </main>
+      <footer></footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
